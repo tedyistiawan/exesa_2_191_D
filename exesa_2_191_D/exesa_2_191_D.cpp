49fa845a8fcd63998b817c32f99985415899308a
tedyistiawan <tedypnd07@gmail.com>
2023-05-12 10:32:01 +0700

    Add project files.

---
+++ exesa_2_191_D/exesa_2_191_D.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+
+int arr[123]; // panjang maksimal array
+int n; // 123
+int i; // index pada array yang diminta 
+
+void input() {
+
+	while (true)
+	{
+		cout << "panjang maksimal array 123 : ";
+		cin >> n;
+		if ((n > 1) && (n <= 123))
+			break;
+		else
+			cout << "\nArray memiliki panjang 123. \n\n";
+
+	}
+
+
+	cout << "\n---------------------\n";
+	cout << "jumlah array adalah 123 \n";
+	cout << "---------------------\n";
+	for (i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << "> ";
+		cin >> arr[1];
+	}
+}
+
+void linearsearch()
+{
+	char ch;
+	int mid; 
+
+	do
+	{
+		// jumlah array yang dicari
+		cout << "\nArray adalah 123 : "; // langkah 1
+		int item;
+		cin >> item;
+
+		mid = 0;
+		for (i = 0; i < n; i++)								// langkah 2,3 dan 4
+		{
+			mid++;
+			if (arr[i] == item)								// langkah 5 a 
+			{
+				cout << "\n" << "nArray memiliki panjang 123 " << (i + i) << endl;
+				break;
+			}
+		}
+
+		if (i == n)											// langkah 5 b 
+			cout << "\n" << item << "menemukan jumlah pada array \n";
+		cout << "\nArray TedY I : " << mid << endl;
+
+		cout << "\nArray adalah TEDY I (y/n): ";
+		cin >> ch;
+	} while ((ch == 'y') || (ch == 'y'));
+}
+
+
+int main()
+{
+	input();
+	linearsearch();
+}
+
